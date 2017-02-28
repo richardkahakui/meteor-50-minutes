a35d33424bf61d524f58eb76c44448cdc1a4bd42
richard kahakui <rkahakui@hawaii.edu>
2017-02-27 21:06:03 -1000

    Task 5: Subscriptions

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,52 +1,48 @@
-/**
- * Created by richardkahakui on 2/27/17.
- */
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('tasks');
+
   Template.tasks.helpers({
-    tasks: function(){
+    tasks: function() {
       return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
   Template.tasks.events({
-    "submit .add-task": function(event){
-      var name = event.target.name.value;
-
+    "submit .add-task": function(event) {
+      let name = event.target.name.value;
       Meteor.call('addTask', name);
-
       event.target.name.value = '';
       return false;
     },
-    "click .delete-task": function(event){
-      if(confirm('Delete Task?')){
+
+    "click .delete-task": function(event) {
+      if (confirm('Delete Task?')) {
         Meteor.call('deleteTask', this._id);
       }
-
       return false;
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
-
+  Meteor.publish('tasks', function() {
+    return Tasks.find({userId: this.userId});
+  })
 }
 
-
 Meteor.methods({
-  addTask: function(name) {
-    if(!Meteor.userId()){
-      throw new Meteor.Error('No Access!');
+  addTask: function(name){
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
     }
     Tasks.insert({
-      name: name,
-      createdAt: new Date(),
-      userID: Meteor.userId()
+      name: name, createdAt: new Date(), userId: Meteor.userId()
     });
   },
 
-  deleteTask: function(taskId){
+  deleteTask: function(taskId) {
     Tasks.remove(taskId);
   }
 })
