716e4ad33e4aef576cb2e3fc993b39d525864c8f
richard kahakui <rkahakui@hawaii.edu>
2017-02-27 20:44:06 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -14,17 +14,14 @@ if (Meteor.isClient) {
     "submit .add-task": function(event){
       var name = event.target.name.value;
 
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
 
       event.target.name.value = '';
       return false;
     },
     "click .delete-task": function(event){
       if(confirm('Delete Task?')){
-        Tasks.remove(this._id);
+        Meteor.call('deleteTask', this._id);
       }
 
       return false;
@@ -35,3 +32,21 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+
+Meteor.methods({
+  addTask: function(name) {
+    if(!Meteor.userId()){
+      throw new Meteor.Error('No Access!');
+    }
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userID: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId){
+    Tasks.remove(taskId);
+  }
+})
